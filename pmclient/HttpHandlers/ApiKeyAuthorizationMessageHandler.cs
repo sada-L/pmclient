b520a03003eb43d3bb1b7d826cbf495acba2967c
sada-L <g-g2g-g78@mail.ru>
2025-04-19 11:55:41 +0300

    feat(api): add handlers

---
+++ pmclient/HttpHandlers/ApiKeyAuthorizationMessageHandler.cs
@@ -0,0 +1,26 @@
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using pmclient.Services;
+using pmclient.Settings;
+
+namespace pmclient.HttpHandlers;
+
+public sealed class ApiKeyAuthorizationMessageHandler : DelegatingHandler
+{
+  private readonly ApiKeySettings _apiKeySettings;
+
+  public ApiKeyAuthorizationMessageHandler(IOptions<ApiKeySettings> apiKeySettings)
+  {
+    _apiKeySettings = apiKeySettings.Value;
+  }
+
+  protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+    CancellationToken cancellationToken)
+  {
+    request.Headers.Add(_apiKeySettings.HeaderName, _apiKeySettings.ApiKey);
+    return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);  
+  }
+}
