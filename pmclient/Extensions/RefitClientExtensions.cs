e89bdd46ffe23ed9b333481b9e20246402e9de22
sada-L <g-g2g-g78@mail.ru>
2025-04-19 11:59:10 +0300

    feat: add extensions

---
+++ pmclient/Extensions/RefitClientExtensions.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Net.Http;
+using Avalonia.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+using pmclient.Helpers;
+using pmclient.HttpHandlers;
+using Refit;
+
+namespace pmclient.Extensions;
+
+public static class RefitClientExtensions
+{
+   public static IServiceCollection AddWebApiClient<TClient, TMessageHandler>(this IServiceCollection services)
+      where TClient : class
+      where TMessageHandler : DelegatingHandler
+   {
+      string baseAddress = ApiEndpoints.BaseAddress;
+
+      services.AddRefitClient<TClient>()
+         .ConfigureHttpClient(config => config.BaseAddress = new Uri(baseAddress))
+         .AddHttpMessageHandler<TMessageHandler>();
+      
+      return services;
+   }
+   
+   public static IServiceCollection AddWebApiClient<TClient>(this IServiceCollection services)
+      where TClient : class
+   {
+      string baseAddress = ApiEndpoints.BaseAddress;
+
+      services.AddRefitClient<TClient>()
+         .ConfigureHttpClient(config => config.BaseAddress = new Uri(baseAddress))
+         .AddHttpMessageHandler<BearerAuthorizationMessageHandler>();
+      
+      return services;
+   }
+}
