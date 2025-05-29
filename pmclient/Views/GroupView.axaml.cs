8a1fa968195b05426c73d9d211f9218b87aa970d
sada-L <g-g2g-g78@mail.ru>
2025-05-29 20:41:56 +0300

    feat(ui): update ui

--- pmclient/Views/GroupView.axaml.cs
+++ pmclient/Views/GroupView.axaml.cs
@@ -1,10 +1,9 @@
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using pmclient.ViewModels;
 
 namespace pmclient.Views;
 
-public partial class GroupView : UserControl
+public partial class GroupView : ReactiveUserControl<GroupViewModel>
 {
     public GroupView()
     {
